3701b2f05f22b8c880116030a4b407118f98a459
davi nicollas <davi.nicollas@hotmail.com>
2021-04-20 11:46:50 -0300

    primeiro commit

---
+++ src/main/java/com/example/wssbd/WebServicesSpringBootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.wssbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringBootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringBootmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wssbd/WebServicesSpringBootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wssbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringBootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
