30f1aa28899f9810cd3c70c02e618638cc6d0da6
davi nicollas <davi.nicollas@hotmail.com>
2021-04-22 09:45:33 -0300

    Usando a entidade user

---
+++ src/main/java/com/example/wssbd/Domain/Comment.java
@@ -0,0 +1,59 @@
+package com.example.wssbd.Domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Comment implements Serializable{
+	private String id;
+	private String text;
+	private Date data;
+	private Post post;
+	private User user;
+	public Comment(String id, String text, Date data) {
+		this.id = id;
+		this.text = text;
+		this.data = data;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Post getPost() {
+		return post;
+	}
+	public void setPost(Post post) {
+		this.post = post;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+		
+}
