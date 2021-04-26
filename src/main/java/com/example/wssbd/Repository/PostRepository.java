1dff3e62e42817966158185e6c38b5034a75f5d9
davi nicollas <davi.nicollas@hotmail.com>
2021-04-26 11:33:34 -0300

    consulta simples com @query~]

--- src/main/java/com/example/wssbd/Repository/PostRepository.java
+++ src/main/java/com/example/wssbd/Repository/PostRepository.java
@@ -3,10 +3,13 @@ package com.example.wssbd.Repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.wssbd.Domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	@Query("{'title': {$regex : ?0, $options: 'i' }}")
+	List<Post> findByTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
