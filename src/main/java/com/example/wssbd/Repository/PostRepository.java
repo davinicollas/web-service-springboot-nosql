b75861946c93cb16f8c294f0009584588b5bac19
davi nicollas <davi.nicollas@hotmail.com>
2021-04-26 11:26:20 -0300

    consulta simples query methods

--- src/main/java/com/example/wssbd/Repository/PostRepository.java
+++ src/main/java/com/example/wssbd/Repository/PostRepository.java
@@ -1,10 +1,12 @@
 package com.example.wssbd.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.wssbd.Domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
