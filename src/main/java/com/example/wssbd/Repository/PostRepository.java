d39555720ceed5f460d540dbcaf03f66f112b343
davi nicollas <davi.nicollas@hotmail.com>
2021-04-28 09:18:53 -0300

    consulta com varios criterios

--- src/main/java/com/example/wssbd/Repository/PostRepository.java
+++ src/main/java/com/example/wssbd/Repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.wssbd.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,4 +13,6 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	@Query("{'title': {$regex : ?0, $options: 'i' }}")
 	List<Post> findByTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{$and : [{date: {$gte: ?1} } , {date: {$lte: ?2} , {$or: [{'title': {$regex : ?0, $options: 'i'}},{'body': {$regex : ?0, $options: 'i'}},{'comment.text': {$regex : ?0, $options: 'i'}}]} ]}")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
